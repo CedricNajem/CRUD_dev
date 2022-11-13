a7262939ac1c445b134a56dc23a5cbf1fa177d6a
CRC <92426463+CedricNajem@users.noreply.github.com>
2022-11-13 21:58:17 +0100

    page adduser ajouter avec css

--- front/src/pages/AddEdit.js
+++ front/src/pages/AddEdit.js
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import axios from "axios";
+import "./AddEdit.css";
+
+const initialState = {
+  name: "",
+  email: "",
+  contact: "",
+};
 
 const AddEdit = () => {
+  const [state, setState] = useState(initialState);
+
+  const { name, email, contact } = state;
+
+  const handleSubmit = (e) => {
+    e.prevnetDefault();
+  };
+
+  const handleInputChange = (e) => {
+    let { name, value } = e.target;
+    setState({ ...state, [name]: value });
+  };
   return (
-    <div>
-      <h2>AddEdit</h2>
+    <div style={{ marginTop: "100px" }}>
+      <form
+        style={{
+          margin: "auto",
+          padding: "15px",
+          maxWidth: "400px",
+          alignCenter: "center",
+        }}
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Jon Doe"
+          onChange={handleInputChange}
+          value={name}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="jondoe@gmail.com"
+          onChange={handleInputChange}
+          value={email}
+        />
+        <label htmlFor="contact">Contact</label>
+        <input
+          type="number"
+          id="contact"
+          name="contact"
+          placeholder="0615151515"
+          onChange={handleInputChange}
+          value={contact}
+        />
+        <input type="submit" value="Add" />
+      </form>
     </div>
   );
 };
