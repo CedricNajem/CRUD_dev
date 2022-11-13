d9ff6fa3764d18859b4ba8190fb2804ebc826a9f
CRC <92426463+CedricNajem@users.noreply.github.com>
2022-11-13 20:28:50 +0100

    Parti back réalisée

--- controllers/user.js
+++ controllers/user.js
@@ -11,3 +11,22 @@ export const createUser = (req, res) => {
   users.push({ ...user, id: uuid() });
   res.send(`User added to the database!`);
 };
+
+export const getUser = (req, res) => {
+  const singleUser = users.filter((user) => user.id === req.params.id);
+  res.send(singleUser);
+};
+
+export const deleteUser = (req, res) => {
+  users = users.filter((user) => user.id !== req.params.id);
+  res.send(`User deleted from the database.`);
+};
+
+export const updateUser = (req, res) => {
+  const user = users.find((user) => user.id === req.params.id);
+  user.name = req.body.name;
+  user.email = req.body.email;
+  user.contact = req.body.contact;
+
+  res.send(`User updated in the database.`);
+};
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { getUsers, createUser } from "../controllers/user.js";
+import {
+  getUsers,
+  createUser,
+  getUser,
+  deleteUser,
+  updateUser,
+} from "../controllers/user.js";
 
 const router = express.Router();
 
 router.get("/users", getUsers);
 router.post("/user", createUser);
+router.get("/user/:id", getUser);
+router.delete("/user/:id", deleteUser);
+router.put("/user/:id", updateUser);
 
 export default router;
